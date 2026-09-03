f8549188027c3b7839ab36e2eda607eac39dea73
koladik <koladik@gmail.com>
2026-09-03 18:10:15 +0300

    update struct

--- scripts_pipeline/task4_validate.py
+++ scripts_pipeline/task4_validate.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import tomllib
 import matplotlib.pyplot as plt
 from pygradskeleton import couprie
 from storage_manager import Storage
@@ -7,11 +7,18 @@ from rocknetmanager.manager_shapefile import label_load, mask_load
 from rocknetmanager.metrics import boundary_f_score
 
 def main():
-	folder_path = Path(r"D:\Data\Outcrops\handmark\IMGP0146")
-	edges_gt_path = Path(r"D:\Data\Outcrops\handmark\IMGP0146\traces_gt\traces.shp")
-	mask_path = Path(r"D:\Data\Outcrops\handmark\IMGP0146\areas")
+	project_path = Path(__file__).resolve().parents[1]
+	config_path = project_path / "config.toml"
+	with config_path.open("rb") as config_file:
+		config = tomllib.load(config_file)
 
-	storage = Storage.from_folder_path(folder_path)
+	folder_validation = Path(config["validation"]["validation_folder"])
+
+	folder_instance = folder_validation / "IMGP0146"
+	edges_gt_path = folder_instance / Path(r"traces_gt\traces.shp")
+	mask_path = folder_instance / Path(r"areas")
+
+	storage = Storage.from_folder_path(folder_instance)
 	image_edges_thin = storage.load_thin_edges()
 	image_mask = mask_load(mask_path, image_edges_thin.shape)
 	image_edges_gt = label_load(
@@ -19,10 +26,10 @@ def main():
 		shape=image_edges_thin.shape,
 		thickness=1
 	)
-	image_edges_gt[image_mask == 0] = 0
-	image_edges_thin[image_mask == 0] = 0
-	f_score = boundary_f_score(edges_pred=image_edges_thin, edges_gt=image_edges_gt, tolerance_px=3)
-	print(f_score)
+	# image_edges_gt[image_mask == 0] = 0
+	# image_edges_thin[image_mask == 0] = 0
+	# f_score = boundary_f_score(edges_pred=image_edges_thin, edges_gt=image_edges_gt, tolerance_px=3)
+	# print(f_score)
 
 	fig = plt.figure(figsize=(14, 9))
 	axs = [fig.add_subplot(1, 2, 1),fig.add_subplot(1, 2, 2)]
